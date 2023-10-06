389b6bc1a9d1ea3589b5ffeb192ad69543013049
Brynne Henderson <bwallace7430@gmail.com>
2023-10-06 12:26:40 -0600

    program to collect all the links from our sites

---
+++ find_pages.py
@@ -0,0 +1,52 @@
+import requests
+import aspose.words as aw
+from bs4 import BeautifulSoup
+
+
+def get_subpages(url, all_links):
+    response = requests.get(url)
+    try:
+        soup = BeautifulSoup(response.content, "html.parser")
+    except Exception as e:
+        print(f"Unable to parse HTML for URL: {url}")
+        return []
+    local_links = []
+
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if (
+            href
+            and (
+                href.startswith("https://nursing.byu.edu/")
+                or href.startswith("https://communications.nursing.byu.edu/")
+                or href.startswith("https://advisement.nursing.byu.edu/")
+            )
+            and href not in all_links
+        ):
+            all_links.append(href)
+            local_links.append(href)
+
+    return local_links
+
+
+def get_pages(url, all_links):
+    local_links = get_subpages(url, all_links)
+    if local_links:
+        builder.write(url.upper() + ": \n")
+        print(url.upper() + ": \n")
+    for i in local_links:
+        builder.write(i + "\n")
+        print(i + "\n")
+    while local_links:
+        get_pages(local_links[0], all_links)
+        local_links.pop(0)
+
+
+doc = aw.Document()
+builder = aw.DocumentBuilder(doc)
+
+main_url = "https://nursing.byu.edu/"
+all_links = []
+
+get_pages(main_url, all_links)
+doc.save("BrightspotPages.docx")
